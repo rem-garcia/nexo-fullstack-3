9c2c8c7c2d127519ad563ff2b7df043f1eb5804f
Remi Garcia Herrera <rem.garcia@duocuc.cl>
2026-05-14 19:55:19 -0400

    fix: remove DashboardLayout from ui package exports

--- packages/ui/index.ts
+++ packages/ui/index.ts
@@ -6,4 +6,3 @@ export { default as InputReclamos } from "./components/InputReclamos";
 export { default as SelectReclamos } from "./components/SelectReclamos";
 export { default as TextAreaReclamos } from "./components/TextAreaReclamos";
 export { AuthImage } from "./components/AuthImage";
-export { DashboardLayout } from './components/DashboardLayout';
